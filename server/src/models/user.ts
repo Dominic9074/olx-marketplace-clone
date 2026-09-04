b40c33bab06298f14d93edf1ebab7b5920ce2e61
dominic9074 <dominickb098@gmail.com>
2026-09-04 20:11:31 +0530

    feat;added the user Model file

---
+++ server/src/models/user.ts
@@ -0,0 +1,33 @@
+import {Schema,model,type Document} from 'mongoose'
+
+
+export interface UserInterface extends Document {
+    name:string,
+    email:string;
+    password:string;
+}
+
+const userSchema=new Schema<UserInterface>(
+    {
+        name:{
+            type:String,
+            required:true,
+            trim:true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true
+        },
+        password:{
+            type:String,
+            required:true
+        }
+    },{timestamps:true}
+)
+
+const UserModel=model<UserInterface>('User',userSchema);
+
+export default UserModel;
