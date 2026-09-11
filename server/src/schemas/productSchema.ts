364a79117637651904c026d687be55f2a207809d
dominic9074 <dominickb098@gmail.com>
2026-09-11 10:35:37 +0530

    update:updated the product schema to the zod latest api version

--- server/src/schemas/productSchema.ts
+++ server/src/schemas/productSchema.ts
@@ -1,11 +1,11 @@
 import {z} from 'zod'
 
 const productSchema =z.object({
-    title:z.string().min(3,'title Should Be At least 3 Character').max(30,'title should not contain more than 30 character'),
-    description:z.string().min(15,'title Should Be At least 15 Character').max(200,'title should not contain more than 200 character'),
-    price:z.number().min(10,'price should be at least 2 digit number'),
-    category:z.string().min(1,'category is required'),
-    imageUrl:z.string().url('invalid image url')
+    title:z.string().min(3,'title Should Be At least 3 Character').max(30,{error:'title should not contain more than 30 character'}),
+    description:z.string().min(15,'title Should Be At least 15 Character').max(200,{error:'title should not contain more than 200 character'}),
+    price:z.number().min(10,{error:'price should be at least 2 digit number'}),
+    category:z.string().min(1,{error:'category is required'}),
+    imageUrl:z.url({error:'invalid image url'})
 })
 
 export default productSchema
