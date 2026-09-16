4bad587b1dc69d860d7ae34e080b0493d69ae9c0
dominic9074 <dominickb098@gmail.com>
2026-09-17 00:34:40 +0530

    feat:added the produt type

---
+++ client/src/types/productType.ts
@@ -0,0 +1,12 @@
+export default interface Product{
+    _id:string;
+    title:string,
+    description:string;
+    price:number;
+    category:string;
+    sellerId:string;
+    imageUrl:string;
+    isSold:boolean;
+    createdAt:string;
+    updatedAt:string;
+}
