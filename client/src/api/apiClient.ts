60ded86561693f07323e4f3d814aea1dabd18cad
dominic9074 <dominickb098@gmail.com>
2026-09-21 14:08:29 +0530

    fix:fixed the base url of the backend api

--- client/src/api/apiClient.ts
+++ client/src/api/apiClient.ts
@@ -3,7 +3,7 @@ import type { RootState } from "../app/store";
 import type { Store } from "@reduxjs/toolkit";
 
 const apiClient = axios.create({
-  baseURL: "https://vercel.com/dominic9074s-projects/olx-marketplace-clone-nbo7",
+  baseURL: "https://olx-marketplace-clone-alpha.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
